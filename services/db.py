c14d6e09b93bef2cadb4b9ad54fd7d61f7472c5a
Lakkam Praharsha <lpraharsha@opentext.com>
2025-08-13 14:03:59 +0530

    Add fix_order API and update db connection

--- services/db.py
+++ services/db.py
@@ -2,8 +2,10 @@ import pyodbc
 
 def get_db_connection():
     return pyodbc.connect(
-        'DRIVER={SQL Server};'
+        'DRIVER=ODBC Driver 18 for SQL Server;'
         'SERVER=DEVDENECOM4.SERVICES.WEBROOT;'
         'DATABASE=ecommerce_vh5;'
-        'Trusted_Connection=yes;'
+        'UID=ecomuser;'
+        'PWD=k74*8lp!;'
+        'TrustServerCertificate=yes;'
     )
